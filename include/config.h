7de00a7bf9f193903d12ce8a63e5de421b910ed7
87projectai-prog <87project.ai@gmail.com>
2026-02-06 08:14:37 +0700

    Create config.h

---
+++ include/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+#define OLED_ADDR 0x3C
+
+// Frame timing
+#define EYE_FRAME_MS 120
+#define NAV_FRAME_MS 100
